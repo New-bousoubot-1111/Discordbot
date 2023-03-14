7e6751b50273bdb7608e01dece214ba691b35d86
New-bousoubot-1111 <127783879+New-bousoubot-1111@users.noreply.github.com>
2023-03-15 03:24:29 +0900

    Update update.py

--- update.py
+++ update.py
@@ -9,69 +9,58 @@ with open('README.md','w') as f:
 with open('json/version.txt','r') as f:
   now_version = f.read()
 new_version = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/json/version.txt").text
-with open('update.py','r') as f:
-  now_version = f.read()
-new_version = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/update.py").text
 
 if now_version != new_version:
   print(Fore.GREEN + f"バージョン{new_version}へのアップデートを確認しました" + Fore.RESET)
   print(Fore.GREEN + "アップデートを適応中です" + Fore.RESET)
+
   #main.py
   with open('json/version.txt','w') as f:
     f.write(new_version)
   with open('main.py','w') as f:
     with open('main.py','r') as f:
-      mainpy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/main.py").text
-      f.write(mainpy)
+      new_code = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/main.py").text
+      f.write(new_code)
   #util.py
   with open('json/version.txt','w') as f:
     f.write(new_version)
   with open('util.py','w') as f:
     with open('util.py','r') as f:
-      utilpy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/util.py").text
-      f.write(utilpy)
-  #update.py
-  with open('json/version.txt','w') as f:
-    f.write(new_version)
-  with open('update.py','w') as f:
-    updatepy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/update.py").text
-    f.write(updatepy)
+      new_code = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/util.py").text
+      f.write(new_code)
+
   #adimin.py
   with open('json/version.txt','w') as f:
     f.write(new_version)
   with open('modules/admin.py','w') as f:
     with open('modules/admin.py','r') as f:
-      adiminpy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/admin.py").text
-      f.write(adiminpy)
+      new_code = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/admin.py").text
+      f.write(new_code)
   #command.py
   with open('json/version.txt','w') as f:
     f.write(new_version)
   with open('modules/command.py','w') as f:
     with open('modules/command.py','r') as f:
-      commandpy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/command.py").text
-      f.write(commandpy)
+      new_code = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/command.py").text
+      f.write(new_code)
   #tasks.py
   with open('json/version.txt','w') as f:
     f.write(new_version)
   with open('modules/tasks.py','w') as f:
     with open('modules/tasks.py','r') as f:
-      taskspy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/tasks.py").text
-      f.write(taskspy)
+      new_code = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/tasks.py").text
+      f.write(new_code)
   #config.json
   with open('json/version.txt','w') as f:
     f.write(new_version)
   with open('json/config.json','w') as f:
     with open('json/config.json','r') as f:
-      configjson = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/json/config.json").text
-      f.write(configjson)
+      new_code = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/json/config.json").text
+      f.write(new_code)
   #help.json
   with open('json/version.txt','w') as f:
     f.write(new_version)
   with open('json/help.json','w') as f:
     with open('json/help.json','r') as f:
-      helpjson = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/json/help.json").text
-      f.write(helpjson)
-
-  print(Fore.GREEN + "アップデートが完了しました" + Fore.RESET)
-else:
-  print(Fore.GREEN + "アップデートはありませんでした" + Fore.RESET)
+      new_code = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/json/help.json").text
+      f.write(new_code)
