fd7cd90d0eab34fd0e73354d01debf07a2f61663
ぼうそうBOT <127783879+New-bousoubot-1111@users.noreply.github.com>
2023-03-14 06:54:51 +0000

    test3

--- modules/command.py
+++ modules/command.py
@@ -1,77 +1,58 @@
 import nextcord
 from nextcord.ext import commands
+import Levenshtein
 import json
-import requests
 from colorama import Fore
-import util
+import os
+import webserver
 
-with open('json/config.json','r') as f:
-	config = json.load(f)
-with open('json/help.json','r') as f:
-	help = json.load(f)
+with open('json/config.json', 'r') as f:
+    config = json.load(f)
 
-color = nextcord.Colour(int(config['color'],16))
+intents = nextcord.Intents.all()
+bot = commands.Bot(
+	command_prefix=config['prefix'],
+	help_command=None,
+        intents=intents
+)
+for module in config['modules']:
+    bot.load_extension(f"modules.{module}")
 
-class command(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
-		
-  @commands.Cog.listener()
-  async def on_ready(self):
-    print(Fore.BLUE + "|command       |" + Fore.RESET)		
-    
-  #ping
-  @commands.command()
-  async def ping(self,ctx):
-        embed=nextcord.Embed(title="ping", description=f"BOTのpingは**{round(self.bot.latency *1000)}**です。",color=color)
+#起動時のログ
+@bot.event
+async def on_ready():
+    print(Fore.GREEN + f"[Ready]\nbot:{bot.user.name}" + Fore.RESET)
+    print(Fore.BLUE + "読み込みファイル" + Fore.RESET)
+    print(Fore.BLUE + "----------------" + Fore.RESET)
+
+#エラー処理
+@bot.event
+async def on_command_error(ctx, error:Exception):
+    if isinstance(error, commands.errors.CommandNotFound):
+        body = ctx.message.content.lstrip(ctx.prefix)
+        res = []
+        for i in bot.commands:
+            check = Levenshtein.distance(str(i), str(body))
+            if check <= 2:
+                res.append(str(i))
+        embed = nextcord.Embed(title="Error",
+                               description=f"コマンドが見つかりません",
+                               color=0xff0000)
+        if not res == []:
+            suggest = "".join([f'`{x}`\n' for x in res])
+            embed.add_field(name="もしかして...", value=suggest)
         await ctx.send(embed=embed)
-    
-  #eew
-  @commands.command()
-  async def eew(self,ctx):
-    request = requests.get(f'https://api.p2pquake.net/v2/history?codes=551&limit=1')
-    response = request.json()[0]
-    data = response['earthquake']
-    hypocenter = data['hypocenter']
-    if request.status_code == 200:
-      embed=nextcord.Embed(title="地震情報",color=color)
-      embed.add_field(name="震源地",value=hypocenter['name'],inline=False)
-      embed.add_field(name="最大震度",value=round(data['maxScale']/10),inline=False)
-      embed.add_field(name="発生時刻",value=data['time'],inline=False)
-      embed.add_field(name="マグニチュード",value=hypocenter['magnitude'],inline=False)
-      embed.add_field(name="震源の深さ",value=f"{hypocenter['depth']}Km",inline=False)
-      await ctx.send(embed=embed)
+    elif isinstance(error, commands.errors.NotOwner):
+        em = nextcord.Embed(title="このコマンドは使用できません",
+                            description="このコマンドは管理者専用です",
+                            color=0xff0000)
+        await ctx.send(embed=em)
     else:
-      await ctx.send("APIリクエストでエラーが発生しました")
-  #eew2
-  @commands.command()
-  async def eew2(self,ctx):
-    request = requests.get("https://api.p2pquake.net/v2/history?codes=551&limit=1")
-    response = request.json()[0]
-    data = response['earthquake']
-    hypocenter = data['hypocenter']
-    if request.status_code == 200:
-      embed=nextcord.Embed(title="地震情報",color=color)
-      embed.add_field(name=f"{data['time']}頃、**{hypocenter['name']}**で地震がありました",value=f"最大震度は**{round(data['maxScale']/10)}**、震源の深さは**{hypocenter['depth']}Km**、マグニチュードは**{hypocenter['magnitude']}**です",inline=False)
-      await ctx.send(embed=embed)
-    else:
-      await ctx.send("APIリクエストでエラーが発生しました")
-  
-  @commands.command()
-  async def help(self,ctx):
-    creators = []
-    for creator in help['owners']:
-      creators.append(await self.bot.fetch_user(int(creator)))
-    creators = "".join(f"\n`{x}`" for x in creators)
-    commands_list = "".join(f"`{help['prefix']}{x}` " for x in help['commands_list'])
-    embed=nextcord.Embed(title="情報",color=color)
-    embed.add_field(name=f"作成者",value=f"{creators}")
-    embed.add_field(name=f"言語",value="Python")
-    embed.add_field(name="",value="\n[公式サイト](https://discord.gg/3VFpmSEugD)")
+        embed=nextcord.Embed(title="エラーが発生しました",description=f"Error ID:`{ctx.message.id}`\n```py\n{error}\n```",color=0xff0000)
+        await ctx.channel.send(embed=embed)
 
-    embed2=nextcord.Embed(title="コマンド",description=f"***{commands_list}***",color=color)
-    await ctx.channel.send(embed=embed,view=util.page_button(embed,embed2))
-    await ctx.channel.send(embed=embed,view=util.info_url(embed))
-
-def setup(bot):
-  return bot.add_cog(command(bot))
+webserver.start()
+try:
+    bot.run(os.getenv("token"))
+except:
+    os.system("kill 1")
--- update.py
+++ update.py
@@ -1,7 +1,4 @@
 import requests
-import json
-from replit import db
-import os
 from colorama import Fore
 
 print(Fore.GREEN + "アップデートを確認しています" + Fore.RESET)
@@ -22,30 +19,18 @@ if now_version != new_version:
   with open('main.py','w') as f:
     mainpy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/main.py").text
     f.write(mainpy)
-  #config.json update
-  with open('json/new.json','w') as f:
-    new_json = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/json/help.json").json()
-    json.dump(new_json,f,indent=4)
-
-  with open('json/help.json','r') as f:
-    config = json.load(f)
-
-  for i in config:
-    db[i] = str(config[i])
-
-  with open('json/new.json','r') as f:
-    new = json.load(f)
-  with open('json/help.json','w') as f:
-    json.dump(new,f,indent=4)
-  
-  for i in db.keys():
-    with open('json/help.json','r') as f:
-      config = json.load(f)
-      config[i] = str(db[i])
-      del db[i]
-    with open('json/help.json','w') as f:
-      json.dump(config,f,indent=4)
-  os.remove("json/new.json")
+  #command.py
+  with open('json/version.txt','w') as f:
+    f.write(new_version)
+  with open('modules/command.py','w') as f:
+    commandpy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/command.py").text
+    f.write(commandpy)
+  #tasks.py
+  with open('json/version.txt','w') as f:
+    f.write(new_version)
+  with open('modules/tasks.py','w') as f:
+    taskspy = requests.get("https://raw.githubusercontent.com/New-bousoubot-1111/Discordbot/main/modules/tasks.py").text
+    f.write(taskspy)
   print(Fore.GREEN + "アップデートが完了しました" + Fore.RESET)
 else:
   print(Fore.GREEN + "アップデートはありませんでした" + Fore.RESET)
